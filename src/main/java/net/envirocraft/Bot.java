040f7fba53de512239e4b8924c97ac405a0edaa0
LemonJoey <JosephRedmond@Hotmail.co.uk>
2022-03-16 20:17:14 +0000

    Changes by bonif

--- src/main/java/net/envirocraft/Bot.java
+++ src/main/java/net/envirocraft/Bot.java
@@ -2,6 +2,9 @@ package net.envirocraft;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.envirocraft.utils.Config;
 
 import javax.security.auth.login.LoginException;
@@ -12,8 +15,9 @@ public class Bot {
 
     public static JDA jda = null;
     public static String localPath = null;
+    public static Guild guild = null;
 
-    public static void main(String[] args) throws LoginException, URISyntaxException {
+    public static void main(String[] args) throws LoginException, URISyntaxException, InterruptedException {
 
         File jarFile = new File(Bot.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
         localPath = jarFile.getParent() + File.separator;
@@ -21,6 +25,36 @@ public class Bot {
         Config.load();
 
         jda = JDABuilder.createDefault(Config.get("token").getAsString())
+                .addEventListeners(new Command())
                 .build();
+
+        jda.awaitReady();
+
+        guild = jda.getGuildById(Config.get("guild").getAsString());
+
+        // TODO: 16/03/2022 Compare against last week
+
+        // TODO: 16/03/2022 Autofill names
+        // TODO: 16/03/2022 User context command
+        assert guild != null;
+        guild.updateCommands()
+                .addCommands(
+                        Commands.slash(
+                                "raiderio",
+                                "Get raider.io information for a player."
+                        ).addOption(
+                                OptionType.STRING,
+                                "name",
+                                "The name of the player.",
+                                true
+                        )
+                )
+                .queue();
+
+        /*
+
+        /raiderio %realm% %name%
+
+         */
     }
 }
